f539966fbb9dc22ac6555a153d77b64d312a490d
anapetro <48393206+anapetro@users.noreply.github.com>
2020-05-10 13:18:08 -0500

    Update 02.variables.php

--- PHP/introPHP/02.variables.php
+++ PHP/introPHP/02.variables.php
@@ -1,35 +1,42 @@
 <?php
 
 $numero  = 5;
-echo "esto es u7n avariable numero: $numero ";
+echo "esto es u7n avariable numero: $numero<br>";
+var_dump($numero);
+
 
 # salto de linea 
 echo "<br></br>";
 
 #variable texto
 $palabra = "palabra";
-echo "esto es una variable texto: $palabra";
+echo "esto es una variable texto: $palabra<br>";
+var_dump($palabra);
 
 # variable Boleana falso y verdadero 
 $boleana = true;
-echo "esto es una variable boleana: $boleana";
+echo "esto es una variable boleana: $boleana<br>";
+var_dump($boleana);
 echo "<br></br>";
 
 # variable arreglo 
 $colores = array("rojo","rosado","morado");
-echo "esto es una variable arreglo: $colore[0]";
+echo "esto es una variable arreglo: $colores[0]<br>";
+var_dump($colores);
 echo "<br></br>";
 
 # variable arreglo con propiedades 
 #(especificar un nombre a cada indice )
 
 $verduras = array("verdura" =>"lechuga", =>"verdura2"=>"cebolla");
-echo "esto es una variable arreglo con propiedades: $verduras[verdura1]";
+echo "esto es una variable arreglo con propiedades: $verduras[verdura1]<br>";
+var_dump($verduras);
 echo "<br></br>";
 
 # variable objeto
 $frutas =(object)["fruta1"=>"pera","fruta2"=>"manzana"];
-echo "esto es una variable objeto: $frutas=>fruta1";
+echo "esto es una variable objeto: $frutas=>fruta1<br>";
+var_dump($frutas);
 
  
 ?>
