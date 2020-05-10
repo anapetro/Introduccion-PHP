84d422d151cbba7296613f166dd11e8c724f9b31
anapetro <48393206+anapetro@users.noreply.github.com>
2020-05-10 15:35:30 -0500

    Create 04.condiciones.php

---
+++ PHP/introPHP/04.condiciones.php
@@ -0,0 +1,23 @@
+<?php
+
+# condiciones 
+  $a = 5;
+  $b= 10;
+
+  if($a > $b){
+      echo "a es mayor que b";
+  }
+
+  else if ($a ==$b){
+    echo "a es igual que b";
+  }
+
+  else {
+    echo "a es menor que b";
+}
+
+
+
+
+
+?>
