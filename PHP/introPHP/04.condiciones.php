70ade62cd795b035c0b08e18aa185bea43dccf14
anapetro <48393206+anapetro@users.noreply.github.com>
2020-05-10 16:07:50 -0500

    Update 04.condiciones.php

--- PHP/introPHP/04.condiciones.php
+++ PHP/introPHP/04.condiciones.php
@@ -4,7 +4,7 @@
   $a = 5;
   $b= 10;
 
-  if($a > $b){
+  if($a > $b){ 
       echo "a es mayor que b";
   }
 
@@ -16,8 +16,30 @@
     echo "a es menor que b";
 }
 
+echo "<br><br>";
+
+# suiches 
+$dia ="sabado";
+switch($dia){
+    case "sabado":
+    echo "voy a estudiar php";
+break;
+
+case "viernes":
+echo "voy a la fiesta ";
+break;
+
+case "domingo":
+echo "voy a descansar";
+break;
+ 
+default: echo "voy a la universidad"; 
+}
 
 
+#ciclo while
+
+$n =1
 
 
 ?>
