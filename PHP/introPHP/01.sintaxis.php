79eee10e2c91250d11dff65fb3d051d27b6c27c8
anapetro <48393206+anapetro@users.noreply.github.com>
2020-05-10 12:32:03 -0500

    Sintaxis, Variables

---
+++ PHP/introPHP/01.sintaxis.php
@@ -0,0 +1,6 @@
+<?php
+
+print"<h1> Mi primir PHP </h1>";
+
+echo "<p><h2>Mi2 codigo</h2></p>","<h3>mi 3 codigo PHP</h3> ";
+?>
---
+++ PHP/introPHP/02.variables.php
@@ -0,0 +1,20 @@
+<?php
+
+$numero  = 5;
+echo "esto es u7n avariable numero: $numero ";
+
+# salto de linea 
+echo "<br></br>";
+
+#variable texto
+$palabra = "palabra";
+echo "esto es una variable texto: $palabra";
+
+# variable Boleana falso y verdadero 
+$boleana = true;
+echo "esto es una variable Texto: $palabra";
+
+
+
+
+?>
